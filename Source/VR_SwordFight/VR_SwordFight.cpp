0ea8c8784093520415e93c84b3628253b1e10c06
Bastian Lang <bluefruitgamesofficial@gmail.com>
2022-04-03 20:33:46 +0200

    Init

---
+++ Source/VR_SwordFight/VR_SwordFight.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VR_SwordFight.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VR_SwordFight, "VR_SwordFight" );
---
+++ Source/VR_SwordFight/VR_SwordFightGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VR_SwordFightGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VR_SWORDFIGHT_API AVR_SwordFightGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
